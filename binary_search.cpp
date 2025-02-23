38a20e342aca5ee10c3a8a55cf57df50a555ef57
Mohamed8Zanaty <mohamed.zanaty8805@gmail.com>
2025-02-24 01:56:33 +0200

    binary_search_rec

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,11 +1,10 @@
-template <class T> int binary_search(vector<T> arr, T target) {
-    int l = 0, r = arr.size() - 1;
-    while (l <= r) {
-        int m = l + (r - l) / 2;
-        if (arr[m] == target) return m;
-        if (arr[m] < target) l = m + 1;
-        else r = m - 1;
-    }
+#include "includes.h"
+template <class T> int binary_search(int l, int r, vector<T> arr, T target) {
+    int mid = (l + r) / 2;
+    if (l > r) return -1;
+    if (arr[mid] == target) return mid;
+    if (arr[mid] < target) return binary_search(mid + 1, r, arr, target);
+    return binary_search(l, mid - 1, arr, target);
     return -1;
 
 }
