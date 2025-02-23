111963e5ff936585dbf424357caeff94dca60c01
Mohamed8Zanaty <mohamed.zanaty8805@gmail.com>
2025-02-24 01:51:09 +0200

    binary_search

---
+++ binary_search.cpp
@@ -0,0 +1,11 @@
+template <class T> int binary_search(vector<T> arr, T target) {
+    int l = 0, r = arr.size() - 1;
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (arr[m] == target) return m;
+        if (arr[m] < target) l = m + 1;
+        else r = m - 1;
+    }
+    return -1;
+
+}
